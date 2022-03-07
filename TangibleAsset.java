34e47951ef4560348e5f98ec5a7a2cb99e2d122b
24mu3 <100890232+24mu3@users.noreply.github.com>
2022-03-07 11:21:04 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,17 @@
+public abstract class TangibleAsset
+{
+  private String name;
+  private int price;
+  private String color;
+  
+  public TangibleAsset(String name, int price, string color)
+  {
+   this.name = name;
+   this.price = price;
+   this.color = color;
+  }
+  
+  public String getName(){retum this.name}
+  public int getPrice(){retum this.price}
+  public String color(){retum this.color}
+}
